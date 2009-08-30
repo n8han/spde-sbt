6a93d14dcae256442d38dc4db7d6cf56dc56873d
Nathan Hamblen <nathan@technically.us>
2009-08-30 18:31:53 -0400

    set publish style to maven

--- project/build/SpdePluginProject.scala
+++ project/build/SpdePluginProject.scala
@@ -1,6 +1,7 @@
 import sbt._
 
 class SpdePluginProject(info: ProjectInfo) extends PluginProject(info) {
+  override def managedStyle = ManagedStyle.Maven
   val publishTo = "Scala Tools Nexus" at "http://nexus.scala-tools.org/content/repositories/releases/"
   Credentials(Path.userHome / ".ivy2" / ".credentials", log)
 }
