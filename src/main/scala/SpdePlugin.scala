15c2ee65ad1657ef6b6e4f625547010b8d0a10d1
Nathan Hamblen <nathan@technically.us>
2009-08-31 23:58:36 -0400

    determine jogl arch from system properties

--- src/main/scala/SpdePlugin.scala
+++ src/main/scala/SpdePlugin.scala
@@ -46,24 +46,35 @@ class SpdeProject(info: ProjectInfo) extends DefaultProject(info) {
 }
 
 class SpdeOpenGLProject(info: ProjectInfo) extends SpdeProject(info) with JoglProject {
-  val opengl = "net.databinder.spde" % "processing-opengl" % spdeVersion.value
+  val opengl = "net.databinder.spde" % "processing-opengl" % spdeVersion.value intransitive()
   override def fork = Some(new ProjectDirectoryRun { 
     override def runJVMOptions = "-Djava.library.path=./lib_managed/compile/" :: Nil
   } )
 }
 
 trait JoglProject extends BasicManagedProject {
-  val jogl_loc: String => String = 
-    "http://download.java.net/media/jogl/builds/archive/jsr-231-1.1.1/jogl-1.1.1-%s.zip" format _
-  val jogl_linux = "net.java.dev" % "jogl-linux" % "1.1.1" from jogl_loc("linux-i586")
-  val jogl_mac = "net.java.dev" % "jogl-mac" % "1.1.1" from jogl_loc("macosx-universal")
+  def jogl_vers = "1.1.1a"
+  val Windows = "Windows(.*)".r
+  def jogl_sig = (
+    (system[String]("os.name").value, system[String]("os.arch").value) match {
+      case ("Linux", "i386") => "linux" :: "i586" :: Nil
+      case ("Mac OS X" | "Darwin", _) => "macosx" :: "universal" :: Nil
+      case (Windows(_), "x86") => "windows" :: "i586" :: Nil
+      case (name, arch) => name :: arch :: Nil
+    }
+  ) map { _.toLowerCase.split(" ").mkString } mkString "-"
+  def jogl_loc = 
+    "http://download.java.net/media/jogl/builds/archive/jsr-231-%s/jogl-%s-%s.zip" format
+      (jogl_vers, jogl_vers, jogl_sig)
+  
+  val jogl = "net.java.dev" % ("jogl-" + jogl_sig) % jogl_vers from jogl_loc
 
   val lib_compile = configurationPath(Configurations.Compile)
 
   override def updateAction = super.updateAction && task {
     import FileUtilities._
     val jogl_zip = outputPath / "jogl_zip"
-    ((lib_compile * "*.zip").get flatMap { file =>
+    ((lib_compile * "jogl-*.zip").get flatMap { file =>
       unzip(file, jogl_zip, "jogl-*/lib/*", log).left.toOption.orElse {
         FileUtilities.clean(file, log)
       } toList
