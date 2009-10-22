7e5e9a65390fbd4d33c9b762f23cd97bbcdbf2e1
Nathan Hamblen <nathan@technically.us>
2009-10-22 12:09:03 -0400

    databinder repository ref for graft plugin

--- graft/src/arc/graft/project/plugins/Plugins.scala
+++ graft/src/arc/graft/project/plugins/Plugins.scala
@@ -1,6 +1,7 @@
 import sbt._
 
 class Plugins(info: ProjectInfo) extends PluginDefinition(info) {
+  val databinder_repo = "Databinder Repository" at "http://databinder.net/repo"
 	val spde_sbt = "us.technically.spde" % "spde-sbt-plugin" % "{{spde-sbt.version}}"
 	val extract = "org.scala-tools.sbt" % "installer-plugin" % "0.2.1"
 }
