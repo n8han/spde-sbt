74431e6b27889645de4e0d2363829137d1d381f4
Nathan Hamblen <nathanh@meetup.com>
2010-03-04 12:04:19 -0500

    correct launcher jar name

--- plugin/src/main/scala/LocalLauncherProject.scala
+++ plugin/src/main/scala/LocalLauncherProject.scala
@@ -9,7 +9,7 @@ trait LocalLauncherProject extends BasicScalaProject {
     val launch_bat = (info.projectPath / "sbt.bat").asFile
     if (System.getProperty("os.name").toLowerCase.indexOf("windows") < 0)
       FileUtilities.write(launch,
-        """cd "`dirname "$0"`"; java -Xmx512M -jar sbt-launcher-%s.jar "$@" """ format sbtVersion.value,
+        """cd "`dirname "$0"`"; java -Xmx512M -jar sbt-launch-%s.jar "$@" """ format sbtVersion.value,
         log
       ) orElse {
         (("chmod" :: "a+x" :: launch.toString :: Nil) ! log)
@@ -17,7 +17,7 @@ trait LocalLauncherProject extends BasicScalaProject {
       }
     else
       FileUtilities.write(launch_bat,
-        "@echo off\njava -Xmx512M -jar sbt-launcher-%s.jar %%*" format sbtVersion.value,
+        "@echo off\njava -Xmx512M -jar sbt-launch-%s.jar %%*" format sbtVersion.value,
         log
       )
   }
