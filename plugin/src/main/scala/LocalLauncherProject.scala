aa89aa0094395b07fa622167166f2883a5771dde
Nathan Hamblen <nathan@technically.us>
2010-03-07 21:33:56 -0500

    lookup jar name for script

--- plugin/src/main/scala/LocalLauncherProject.scala
+++ plugin/src/main/scala/LocalLauncherProject.scala
@@ -7,18 +7,21 @@ trait LocalLauncherProject extends BasicScalaProject {
     import Process._
     val launch = (info.projectPath / "sbt").asFile
     val launch_bat = (info.projectPath / "sbt.bat").asFile
-    if (System.getProperty("os.name").toLowerCase.indexOf("windows") < 0)
-      FileUtilities.write(launch,
-        """cd "`dirname "$0"`"; java -Xmx512M -jar sbt-launch-%s.jar "$@" """ format sbtVersion.value,
-        log
-      ) orElse {
-        (("chmod" :: "a+x" :: launch.toString :: Nil) ! log)
-        None
-      }
-    else
-      FileUtilities.write(launch_bat,
-        "@echo off\njava -Xmx512M -jar sbt-launch-%s.jar %%*" format sbtVersion.value,
-        log
-      )
+    (path(".") * "sbt-launch-*.jar").get.foreach { launch_jar =>
+      if (System.getProperty("os.name").toLowerCase.indexOf("windows") < 0)
+        FileUtilities.write(launch,
+          """cd "`dirname "$0"`"; java -Xmx512M -jar %s "$@" """ format launch_jar,
+          log
+        ) orElse {
+          (("chmod" :: "a+x" :: launch.toString :: Nil) ! log)
+          None
+        }
+      else
+        FileUtilities.write(launch_bat,
+          "@echo off\njava -Xmx512M -jar %s %%*" format launch_jar,
+          log
+        )
+    }
+    None
   }
 }
