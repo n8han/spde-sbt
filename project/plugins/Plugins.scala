dfcbe100f84525406509a1c45f56fa325635a2ca
Nathan Hamblen <nathan@technically.us>
2010-08-22 13:16:41 -0400

    plugin versions

--- project/plugins/Plugins.scala
+++ project/plugins/Plugins.scala
@@ -4,5 +4,6 @@ class Plugins(info: ProjectInfo) extends PluginDefinition(info) {
   val archetect = "net.databinder" % "archetect-plugin" % "0.1.3"
 
   val t_repo = "t_repo" at "http://tristanhunt.com:8081/content/groups/public/"
-	val posterous = "net.databinder" % "posterous-sbt" % "0.1.4"
+  val snuggletex_repo = "snuggletex_repo" at "http://www2.ph.ed.ac.uk/maven2"
+  val posterous = "net.databinder" % "posterous-sbt" % "0.1.6"
 }
